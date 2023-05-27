3334b0a7e9de761d9555d31f86105d24c8e1c0df
Nguyễn Tấn Dũng <tandung140202@gmail.com>
2023-05-27 10:15:09 +0700

    abc

--- models/userModel.js
+++ models/userModel.js
@@ -1,34 +1,55 @@
 const mongoose = require('mongoose'); // Erase if already required
 const bcrypt = require('bcrypt');
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-    fistname: {
-        type: String,
-        required: true,
+var userSchema = new mongoose.Schema(
+    {
+        fistname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        mobile: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            default: 'user',
+        },
+        cart: {
+            type: Array,
+            default: [],
+        },
+        address: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'address',
+            },
+        ],
+        wishList: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+            },
+        ],
     },
-    lastname: {
-        type: String,
-        required: true,
+    {
+        timestamps: true,
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    mobile: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        default: 'user',
-    },
-});
+);
 
 userSchema.pre('save', async function (next) {
     const salt = await bcrypt.genSaltSync(10);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updateUser } = require('../controller/userCtrl');
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddlewares');
 
 router.post('/register', createUser);
 router.post('/login', loginUserCtrl);
 router.get('/all-users', getallUser);
-router.get('/:id', getaUser);
+router.get('/:id', authMiddleware, isAdmin, getaUser);
 router.delete('/:id', deleteaUser);
 router.put('/:id', updateUser);
 
